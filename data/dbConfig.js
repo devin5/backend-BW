3b719a5784eedd116208e185a1fe663f05adde13
Devin Dias <devindias.tech@gmail.com>
2019-11-16 12:26:04 -0600

    practice db

---
+++ data/dbConfig.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+const env = process.env.DB_ENV ||'development';
+
+module.exports = knex(knexConfig[env]);
+
+
---
+++ knexfile.js
@@ -0,0 +1,41 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/datbase.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+      migrations: {
+        directory: './data/migrations',
+      },
+
+  },
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/datbase.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+      migrations: {
+        directory: './data/migrations',
+      },
+
+  },
+}
+
+
+  
+ 
